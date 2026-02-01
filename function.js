7beccd8d20185746d16694ee89a8e45959e34ebd
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-02-01 05:29:21 +0100

    upgraded logic, fixed small issues/bugs

--- function.js
+++ function.js
@@ -3,52 +3,66 @@ let currentNumber = "";
 let previousNumber = "";
 let operator = null;
 let result = "";
+let justCalculated = false;
 
 variables.forEach(button => {
     button.addEventListener("click", () => {
         const value = button.textContent;
     if (!isNaN(value)) {
         if (currentNumber.length >= 7) return;
+        if (justCalculated) return;
         currentNumber += value;
         updateDisplay(currentNumber);
         return;
          
 } if (["+","-","*","/"].includes(value)) {
-        previousNumber = currentNumber;
+        if (justCalculated) {
+            previousNumber = result;
+        } else {
+            previousNumber = currentNumber;
+        }
         operator = value;
         currentNumber = "";
         updateMiniDisplay(previousNumber);
+        justCalculated = false;
         return;
 
     } if (value === "=") {
-        result = calculate(previousNumber, operator, currentNumber);   
+        result = calculate(previousNumber, operator, currentNumber);
+        previousNumber = result;
+        currentNumber = "";   
         updateDisplay(result); 
         updateMiniDisplay(result);
-        return; 
-        
+        updateCalcHistory();
+        justCalculated = true;
+        return;
+
     } if (value === "⌫") {
-        currentNumber = currentNumber.slice(0, -1)
+        if (justCalculated || currentNumber === "") return;
+        currentNumber = currentNumber.slice(0, -1) 
         updateDisplay(currentNumber);
         return;
-    }    
-});
-});
-    
+    }        
+})});
+
 function calculate(previousNumber, operator , currentNumber) {
-            previousNumber = Number(previousNumber)
-            currentNumber = Number(currentNumber)
+            previousNumber = Number(previousNumber);
+            currentNumber = Number(currentNumber);
 
             switch(operator) {
-                case "+": return previousNumber + currentNumber;
-                case "-": return previousNumber - currentNumber;
-                case "*": return previousNumber * currentNumber;
-                case "/": return previousNumber / currentNumber; 
-                
+                case "+": result = previousNumber + currentNumber; break;
+                case "-": result = previousNumber - currentNumber; break;
+                case "*": result = previousNumber * currentNumber; break;
+                case "/": result = previousNumber / currentNumber; break;  
             }
+            return result;
         }
         function updateDisplay(value) {              
             document.querySelector("#display").textContent = value; 
         }
         function updateMiniDisplay() {
-            document.querySelector("#displayMini").textContent = previousNumber + operator + currentNumber;
+            document.querySelector("#displayMini").textContent = "figuring it out";
+        }
+        function updateCalcHistory() {
+            document.querySelector("#memoryCalc").textContent = "figuring it out, yes";
         }
