b5261ae9edf172c7e5e5525a3062b5569176d4de
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-02-01 01:50:15 +0100

    add user input, backspace, first part calc func

--- function.js
+++ function.js
@@ -1,12 +1,29 @@
-const add = function(a, b){
 
-}
-const subtract = function(a, b) {
-    
-}
-const multiply = function(a, b) {
+const variables = document.querySelectorAll('button');
+let currentNumber = "";
+let previousNumber = "";
+let operator = null;
 
-}
-const divide = function(a, b) {
+variables.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
+    if (!isNaN(value)) {
+        currentNumber += value;
+        updateDisplay(currentNumber);
+        return;
 
-}
+    } if (["+","-","*","/"].includes(value)) {
+        previousNumber = currentNumber;
+        operator = value;
+        currentNumber = "";
+        console.log(previousNumber)
+
+    } if (value === "⌫") {
+        currentNumber = currentNumber.slice(0, -1)
+        updateDisplay(currentNumber);
+        return;
+    }
+     function updateDisplay(value) {
+            document.querySelector("#display").textContent = value;
+     }
+})})
